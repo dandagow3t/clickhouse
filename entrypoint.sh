d917411341f3f5c985dd4f9e25129704ba575f65
dandagow3t <arcassis@gmail.com>
2025-02-17 15:08:58 +0200

    Some fixes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Set permissions for the data directory
+# Set permissions for all required directories
 chown -R clickhouse:clickhouse /var/lib/clickhouse
+chown -R clickhouse:clickhouse /var/log/clickhouse-server
+chown -R clickhouse:clickhouse /etc/clickhouse-server
 
 # Only process users.xml template since config.xml is now static
 envsubst < /etc/clickhouse-server/users.xml.template > /etc/clickhouse-server/users.xml
